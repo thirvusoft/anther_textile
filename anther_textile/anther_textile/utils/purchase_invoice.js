c21ed7f9f69723247baf27d4c76f90ed3ae63217
creative-paramu <pparameshwari@thirvusoft.in>
2023-09-30 15:52:17 +0530

    print-format

--- anther_textile/anther_textile/utils/purchase_invoice.js
+++ anther_textile/anther_textile/utils/purchase_invoice.js
@@ -4,7 +4,7 @@ frappe.ui.form.on('Purchase Invoice', {
         var item_price = frappe.model.get_new_doc('Item Price')
         item_price.item_code = row.item_code
         item_price.price_list = "Standard Selling" 
-        item_price.batch_no = row.batch_no 
+        item_price.batch_no = row.batch_no || ""
         item_price.price_list_rate = row.custom_mrp 
         item_price.valid_from = frm.doc.posting_date   
         frappe.call({method:"frappe.client.save", args:{doc:item_price}})
